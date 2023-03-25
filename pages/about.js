2fb7fa6ede688165aa5290c6caec3b65a0d925b4
notsosimppole <gitansh1208@gmail.com>
2023-03-25 19:31:34 +0530

    apos

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ function About() {
         <div className="bg-[#996A16] ">
             <div className="flex flex-col pl-12 pt-12 w-[55vw]">
                 <p className="stencil text-4xl text-[white] pb-8">About</p>
-                <p className="font-inter text-lg pb-20 text-white"> <span className="text-black font-bold">Aahvaan</span> is North India’s largest sports festival conducted by Department of Physical Education & Sports, Delhi Technological University. From 20+ sports events to cultural events and EDM nights Aahvaan has it all.<br></br>
+                <p className="font-inter text-lg pb-20 text-white"> <span className="text-black font-bold">Aahvaan</span> is North India&apos;s largest sports festival conducted by Department of Physical Education & Sports, Delhi Technological University. From 20+ sports events to cultural events and EDM nights Aahvaan has it all.<br></br>
                     <br></br>
                     The word Aahvaan means <span className="text-black font-bold">‘Call to Challenge’</span>  in hindi and the festival lives up to its name by providing a platform for students to showcase their skills in 20+ sports events, develop their sporting skills, and interact with peers from different college & universities.
                     <br></br>
